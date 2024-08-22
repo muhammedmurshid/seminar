fee0345b28b73e9ba9d6927c78120685b35a12b2
murshid <muhsink00000@gmail.com>
2024-08-22 16:36:07 +0530

    add new data for academic year 2023-24

--- models/leads.py
+++ models/leads.py
@@ -48,7 +48,7 @@ class SeminarLeads(models.Model):
                                readonly=True, default=lambda self: _('New'))
     count_duplicate = fields.Integer(string='Count Duplicate', compute='_compute_count_duplicate', store=True)
     bulk_lead_assign = fields.Boolean(string='Bulk Lead Assign')
-    academic_year = fields.Selection([('2024', '2024-25'), ('2025', '2025-26')], string='Academic Year', required=1)
+    academic_year = fields.Selection([('2023', '2023-24'), ('2024', '2024-25'), ('2025', '2025-26')], string='Academic Year', required=1)
 
     def _compute_get_lead_manager(self):
         print('kkkll')
